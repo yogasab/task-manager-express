d8e70fe682e7a314f8303166e9bcbe3079a596e6
Yoga Baskoro <baskoroyoga40@gmail.com>
2021-12-14 18:42:17 +0700

    Create route to show all users and tasks

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,16 +1,3 @@
 const mongoose = require("mongoose");
-// const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api");
-
-// const Task = mongoose.model("Task", {
-// 	description: {
-// 		type: String,
-// 		trim: true,
-// 		required: true,
-// 	},
-// 	completed: {
-// 		type: Boolean,
-// 		default: false,
-// 	},
-// });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../src/models/user");
+const Task = require("../src/models/task");
 require("../src/db/mongoose");
 
 const app = express();
@@ -12,9 +13,62 @@ app.post("/users", (req, res) => {
 	user
 		.save()
 		.then(() => {
-			res.send(user);
+			res.status(201).send(user);
 		})
 		.catch((err) => res.status(400).send(err));
+});
+
+app.get("/users", (req, res) => {
+	User.find()
+		.then((users) => {
+			res.status(200).send(users);
+		})
+		.catch((err) => res.status(500).send(err));
+});
+
+app.get("/users/:id", (req, res) => {
+	const _id = req.params.id;
+	User.findById(_id)
+		.then((user) => {
+			if (!user) {
+				return res.status(404).send();
+			}
+			res.status(200).send(user);
+		})
+		.catch((err) => res.status(500).send(err));
+});
+
+app.post("/tasks", (req, res) => {
+	const task = new Task(req.body);
+	task
+		.save()
+		.then(() => {
+			res.status(201).send(task);
+		})
+		.catch((err) => {
+			res.status(400).send(err);
+		});
+});
+
+app.get("/tasks", (req, res) => {
+	Task.find()
+		.then((tasks) => {
+			res.status(200).send(tasks);
+		})
+		.catch((err) => {
+			res.status(500).send();
+		});
+});
+
+app.get("/tasks/:id", (req, res) => {
+	const _id = req.params.id;
+	Task.findById(_id)
+		.then((task) => {
+			res.status(200).send(task);
+		})
+		.catch((err) => {
+			res.status(404).send();
+		});
 });
 
 app.listen(port, () => {
