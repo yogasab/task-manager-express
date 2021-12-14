292c1bec4a06cb4b55181a9d9d6ab5e7ebe74861
Yoga Baskoro <baskoroyoga40@gmail.com>
2021-12-14 14:05:33 +0700

    Create POST controller to create a nes user

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const User = require("../src/models/user");
+require("../src/db/mongoose");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
+app.post("/users", (req, res) => {
+	const user = new User(req.body);
+	user
+		.save()
+		.then(() => {
+			res.send(user);
+		})
+		.catch((err) => res.status(400).send(err));
+});
+
 app.listen(port, () => {
-	console.log(`Listenin to ${port}`);
+	console.log(`Listening to ${port}`);
 });
---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+// User Model
+const User = mongoose.model("User", {
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+		lowercase: true,
+		validate(value) {
+			if (!validator.isEmail(value)) {
+				throw new Error("Email is invalid");
+			}
+		},
+	},
+	age: {
+		type: Number,
+		default: 0,
+		validate(value) {
+			if (value < 0) {
+				throw new Error("Age must be greater than zero");
+			}
+		},
+	},
+	password: {
+		type: String,
+		required: true,
+		minLength: 7,
+		trim: true,
+		validate(value) {
+			if (value.toLowerCase().includes("password")) {
+				throw new Error("Cannot use 'password' as password value");
+			}
+		},
+	},
+});
+
+module.exports = User;
