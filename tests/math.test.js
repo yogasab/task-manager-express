0747aa36f0ae407c5f3159d858223368188808c5
Yoga Baskoro <baskoroyoga40@gmail.com>
2021-12-23 14:40:02 +0700

    Create unit test to test method in math.js

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,5 +1,28 @@
-test("Hello World", () => {});
+const {
+	calculateTip,
+	fahrenheitToCelcius,
+	celciusToFahrenheit,
+} = require("../src/test/math");
 
-test("Failure test", () => {
-	throw new Error("Failure");
+test("Calculate total amount plus tip", () => {
+	const total = calculateTip(10, 0.3);
+	expect(total).toBe(13);
+	// if (total !== 13) {
+	// 	throw new Error("The result should be 13, got " + total);
+	// }
+});
+
+test("Calculate total amount with default tip", () => {
+	const total = calculateTip(10);
+	expect(total).toBe(12);
+});
+
+test("Convert temperature from Fahrenheit to Celcius", () => {
+	const value = fahrenheitToCelcius(32);
+	expect(value).toBe(0);
+});
+
+test("Convert temperature from Celcius to Fahrenheit", () => {
+	const value = celciusToFahrenheit(10);
+	expect(value).toBe(0.5625);
 });
